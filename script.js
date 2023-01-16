e7c6c97e472aa20fbc90e582377fd0e3211c7fed
Sam Hernandez <samd.hernandezc24@gmail.com>
2023-01-16 00:30:01 -0600

    adding a popup modal box

---
+++ script.js
@@ -0,0 +1,13 @@
+const section = document.querySelector('section');
+const hireBtn = section.querySelector('#hireBtn');
+const closeBtn = section.querySelectorAll('#close');
+
+hireBtn.addEventListener('click', () => {
+  section.classList.add('show');
+});
+
+closeBtn.forEach((cBtn) => {
+  cBtn.addEventListener('click', () => {
+    section.classList.remove('show');
+  });
+});
